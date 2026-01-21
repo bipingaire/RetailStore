739f3e0baa59540421c54fb596108a7a0a92d3fc
Anuj12Pokharel <anujpokharel.076@kathford.edu.np>
2026-01-22 01:54:31 +0545

    Remove: 20260109_delete_broken_user.sql

--- supabase/migrations/20260109_delete_broken_user.sql
+++
@@ -1,9 +0,0 @@
--- DELETE BROKEN USER TO ALLOW RE-SIGNUP
--- We delete the user with the known ID so you can sign up again with a fresh password.
-
-DELETE FROM auth.users
-WHERE id = 'dfeaeff6-3aa3-40d5-9674-7a51d41f6945';
-
--- Also clean up the superadmin table entry if it exists to avoid conflicts
-DELETE FROM "superadmin-users"
-WHERE "user-id" = 'dfeaeff6-3aa3-40d5-9674-7a51d41f6945';
